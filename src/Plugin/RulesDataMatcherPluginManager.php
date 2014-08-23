4d43cc0ee3a0600be9bd8af0ba3e1361e36313ff
omissis <claudi0.beatric3@gmail.com>
2014-08-23 02:42:21 +0200

    Fixed RulesDataMatcherPluginManager's annotation name constructor argument.

--- src/Plugin/RulesDataMatcherPluginManager.php
+++ src/Plugin/RulesDataMatcherPluginManager.php
@@ -18,7 +18,7 @@ class RulesDataMatcherPluginManager extends DefaultPluginManager {
   /**
    * {@inheritdoc}
    */
-  public function __construct(\Traversable $namespaces, ModuleHandlerInterface $module_handler, $plugin_definition_annotation_name = 'Drupal\rules\Annotation\RulesExpression') {
+  public function __construct(\Traversable $namespaces, ModuleHandlerInterface $module_handler, $plugin_definition_annotation_name = 'Drupal\rules\Annotation\RulesDataMatcher') {
     parent::__construct('Plugin/Matcher', $namespaces, $module_handler, $plugin_definition_annotation_name);
     $this->alterInfo('rules_datamatcher');
   }
