1c9547912e36e3940c07db035f651c4add6e7b32
omissis <claudi0.beatric3@gmail.com>
2014-09-29 13:14:37 +0100

    Fixed parent constructor in rules data matcher plugin manager.

--- src/Plugin/RulesDataMatcherPluginManager.php
+++ src/Plugin/RulesDataMatcherPluginManager.php
@@ -19,8 +19,9 @@ class RulesDataMatcherPluginManager extends DefaultPluginManager {
    * {@inheritdoc}
    */
   public function __construct(\Traversable $namespaces, ModuleHandlerInterface $module_handler, $plugin_definition_annotation_name = 'Drupal\rules\Annotation\RulesDataMatcher') {
-    parent::__construct('Plugin/Matcher', $namespaces, $module_handler, $plugin_definition_annotation_name);
     $this->alterInfo('rules_datamatcher');
+
+    parent::__construct('Plugin/Matcher', $namespaces, $module_handler, NULL, $plugin_definition_annotation_name);
   }
 
 }
